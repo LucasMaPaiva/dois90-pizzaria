c9622bc0914e66d94cf37293543ff4a056356046
paiva <lucaspaivadev@gmail.com>
2026-03-25 20:10:33 -0400

    ajustando primeira foto]

---
+++ src/components/WhatsAppPromo.jsx
@@ -0,0 +1,5 @@
+export default function WhatsAppPromo() {
+  return (
+    <div id="b_3536911_1_177340945037788578" className="gpc-b "><div className="gpc-b_sobreposicao"></div><div className="centralizar"><div id="e_3536911_1_177340960390076055" className="gpc-e e_botao dd dm e_3536911_1_177340960390076055"><a className="c borda_igual e_botao link_externo" href="https://wa.me/559536218600">WHATSAPP</a></div><div id="e_3536911_1_177340951272221644" className="gpc-e e_texto dd dm e_3536911_1_177340951272221644"><div className="c e_texto"><p><span>Peça pelo cardápio direto e acumule pontos de fidelidade a cada compra. <span>Cada pedido conta.</span></span></p></div></div></div></div>
+  );
+}
