76af89b851862edb820aef57dc668f5d1dbe74cd
paiva <lucaspaivadev@gmail.com>
2026-04-02 16:46:47 -0400

    ajustes

--- src/components/VisitUs.jsx
+++ src/components/VisitUs.jsx
@@ -6,25 +6,25 @@ const units = [
     id: 'pizzaria-aeroporto',
     name: 'Pizzaria dois90 - Aeroporto',
     address: 'R. Yeyê Coelho, 580A - Aeroporto, Boa Vista - RR',
-    mapUrl: 'https://maps.google.com/maps?q=Pizzaria%20Dois90%20Aeroporto%20Boa%20Vista%20RR&t=&z=15&ie=UTF8&iwloc=&output=embed'
+    mapUrl: 'https://www.google.com/maps?q=Pizzaria%20Dois90%20Aeroporto%20Boa%20Vista%20RR&t=&z=15&ie=UTF8&iwloc=&output=embed'
   },
   {
     id: 'pizzaria-cacari',
     name: 'Pizzaria dois90 - Caçari',
     address: 'Av. Ville Roy, 2185 - Caçari, Boa Vista - RR',
-    mapUrl: 'https://maps.google.com/maps?q=Pizzaria%20Dois90%20Caçari%20Boa%20Vista%20RR&t=&z=15&ie=UTF8&iwloc=&output=embed'
+    mapUrl: 'https://www.google.com/maps?q=Pizzaria%20Dois90%20Caçari%20Boa%20Vista%20RR&t=&z=15&ie=UTF8&iwloc=&output=embed'
   },
   {
     id: 'gelateria-aeroporto',
     name: 'Gelateria dois90 - Aeroporto',
     address: 'R. Yeyê Coelho, 580A - Aeroporto, Boa Vista - RR',
-    mapUrl: 'https://maps.google.com/maps?q=Gelateria%20Dois90%20Aeroporto%20Boa%20Vista%20RR&t=&z=15&ie=UTF8&iwloc=&output=embed'
+    mapUrl: 'https://www.google.com/maps?q=Gelateria%20Dois90%20Aeroporto%20Boa%20Vista%20RR&t=&z=15&ie=UTF8&iwloc=&output=embed'
   },
   {
     id: 'gelateria-aparecida',
     name: 'Gelateria dois90 - Aparecida',
     address: 'R. José Bonifácio, 504 - Aparecida, Boa Vista - RR',
-    mapUrl: 'https://maps.google.com/maps?q=Gelatos%20Dois90%20Aparecida%20Boa%20Vista%20RR&t=&z=15&ie=UTF8&iwloc=&output=embed'
+    mapUrl: 'https://www.google.com/maps?q=Gelatos%20Dois90%20Aparecida%20Boa%20Vista%20RR&t=&z=15&ie=UTF8&iwloc=&output=embed'
   }
 ];
 
